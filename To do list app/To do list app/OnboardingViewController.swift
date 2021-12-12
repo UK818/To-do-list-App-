4beafd0d3153a860f5dbf369003e2011f3400b12
Usman Kulaha <32143087+UK818@users.noreply.github.com>
2021-12-12 10:53:25 +0100

    created Onboarding view controller

---
+++ To do list app/To do list app/OnboardingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  To do list app
+//
+//  Created by mac on 11/12/2021.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+	@IBOutlet weak var nameTextField: UITextField!
+	
+	
+	@IBOutlet weak var nextButton: UIButton!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+
+	@objc func didTapNext () {
+		
+//		let homeVC =
+		
+	}
+	
+}
+
